ee92b832234eb01dc802f06c9913f8af5de30b78
shdwraze <shdwrazedev@gmail.com>
2023-10-23 18:06:55 +0300

    add: search text field

--- app/src/main/java/com/shdwraze/flightsearch/FlightSearchApp.kt
+++ app/src/main/java/com/shdwraze/flightsearch/FlightSearchApp.kt
@@ -1,12 +1,21 @@
 package com.shdwraze.flightsearch
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shdwraze.flightsearch.ui.component.FlightSearchTopAppBar
+import com.shdwraze.flightsearch.ui.component.SearchField
 import com.shdwraze.flightsearch.ui.theme.FlightSearchTheme
 
 @Composable
@@ -16,10 +25,22 @@ fun FlightSearchApp() {
             FlightSearchTopAppBar()
         }
     ) { innerPadding ->
-        Text(
-            text = "Hello",
+        var textState by remember { mutableStateOf(TextFieldValue("")) }
+
+        Surface(
             modifier = Modifier.padding(innerPadding)
-        )
+        ) {
+            SearchField(
+                modifier = Modifier.padding(PaddingValues(16.dp)),
+                textState = textState,
+                onCloseButtonClick = {
+                    textState = TextFieldValue("")
+                },
+                onValueChange = {
+                    textState = it
+                }
+            )
+        }
     }
 }
 
---
+++ app/src/main/java/com/shdwraze/flightsearch/ui/component/SearchField.kt
@@ -0,0 +1,61 @@
+package com.shdwraze.flightsearch.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.shdwraze.flightsearch.ui.theme.FlightSearchTheme
+
+@Composable
+fun SearchField(
+    modifier: Modifier = Modifier,
+    textState: TextFieldValue = TextFieldValue(""),
+    onCloseButtonClick: (TextFieldValue) -> Unit = {},
+    onValueChange: (TextFieldValue) -> Unit = {}
+) {
+    TextField(
+        value = textState,
+        onValueChange = { onValueChange(it) },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        },
+        placeholder = { Text("Enter departure airport") },
+        shape = RoundedCornerShape(36.dp),
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        trailingIcon = {
+            if (textState.text.isNotEmpty()) {
+                IconButton(onClick = { onCloseButtonClick(textState) }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Preview
+@Composable
+fun SearchPrev() {
+    FlightSearchTheme {
+        SearchField()
+    }
+}
